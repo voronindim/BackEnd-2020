bf5488a962cfb45c1d5ce9d5d1021b45ff1f953a
dmitrii <VoronindimTvink123@mail.ru>
2020-03-21 15:47:08 +0300

    add lab2

---
+++ lab2/lab_2/PrintArgs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace PrintArgs
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 0)
+            {
+                Console.WriteLine("Number of arguments: " + args.Length);
+                Console.WriteLine("Arguments: " + string.Join(" ", args));
+            }
+            else
+            {
+                Console.WriteLine("No parameters were specified!");
+            }
+        }
+    }
+}
---
+++ lab2/lab_2/RemoveDuplicates/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System;
+
+namespace RemoveDuplicates
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Incorrect number of arguments\n" +
+                                  "Usage remove_duplicates.exe < input string >");
+            }
+            else
+            {
+                Console.Write(args[0].Distinct().ToArray());
+            }
+        }
+    }
+}
