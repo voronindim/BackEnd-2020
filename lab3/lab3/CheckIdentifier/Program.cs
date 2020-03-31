b70d3f3ff0686944b1e68ffec664b5b50173bbd2
dmitrii <VoronindimTvink123@mail.ru>
2020-03-31 17:21:49 +0300

    CheckIdentifier

---
+++ lab3/lab3/CheckIdentifier/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace CheckIdentifier
+{
+    
+    internal class Program
+    {
+        public static bool CheckIdentifier(string ident)
+        {
+            if (ident[0] >= 'A' && ident[0] <= 'Z' || ident[0] >= 'a' && ident[0] <= 'z')
+            {
+                for (int i = 1; i < ident.Length; i++)
+                {
+                    if (!(ident[i] >= 'A' && ident[i] <= 'Z' || ident[i] >= 'a' && ident[i] <= 'z' ||
+                        ident[i] >= '1' && ident[i] <= '9'))
+                    {
+                        Console.WriteLine("No");
+                        Console.WriteLine("Встречен неизвестный символ в строке" + "\'" + ident[i] + "\'");
+                        return false;
+                    }
+                }
+            }
+            Console.WriteLine("Yes");
+            return true;
+        }
+        
+        public static bool ParseArg(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+        
+        public static int Main(string[] args)
+        {
+            if (!ParseArg(args))
+            {
+                return 1;
+            }
+
+            string ident = args[0];
+
+            if (!CheckIdentifier(ident))
+            {
+                return 1;
+            }
+            return 1;
+        }
+    }
+}
