62ba661ebd44cc2eeba23d8af2fff24cf14524c1
dmitrii <VoronindimTvink123@mail.ru>
2020-04-04 21:20:05 +0300

    add PasswordStrength

---
+++ lab3/PasswordStrength/CheckPasswordStrengthTests/UnitTest1.cs
@@ -0,0 +1,100 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PasswordStrength;
+
+namespace CheckPasswordStrengthTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CalculateByLength_with_correct_password()
+        {
+            string password = "ab1";
+            int sum = 12;
+            Assert.AreEqual(sum, Program.CalculateByLenght(ref password));
+
+            password = "";
+            sum = 0;
+            Assert.AreEqual(sum, Program.CalculateByLenght(ref password));
+        }
+
+        [TestMethod]
+        public void CalculateByDigitsAmount_()
+        {
+            string password = "abc11";
+            int sum = 8;
+            Assert.AreEqual(sum, Program.CalculateByDigitsAmount(ref password));
+
+
+            password = "";
+            sum = 0;
+            Assert.AreEqual(sum, Program.CalculateByLenght(ref password));
+        }
+
+        [TestMethod]
+        public void CalculateByUpperLettersAmount_()
+        {
+            string password = "aAc11";
+            int sum = 8;
+            Assert.AreEqual(sum, Program.CalculateByUpperLettersAmount(ref password));
+
+
+            password = "";
+            sum = 0;
+            Assert.AreEqual(sum, Program.CalculateByUpperLettersAmount(ref password));
+        }
+
+        [TestMethod]
+        public void CalculateLowerCaseLettersAmount_()
+        {
+            string password = "AcA11";
+            int sum = 8;
+            Assert.AreEqual(sum, Program.CalculateLowerCaseLettersAmount(ref password));
+
+
+            password = "";
+            sum = 0;
+            Assert.AreEqual(sum, Program.CalculateLowerCaseLettersAmount(ref password));
+        }
+
+
+        [TestMethod]
+        public void CalculateByOnlyDigits_()
+        {
+            string password = "1122";
+            int sum = -4;
+            Assert.AreEqual(sum, Program.CalculateByOnlyDigits(ref password));
+
+
+            password = "";
+            sum = 0;
+            Assert.AreEqual(sum, Program.CalculateByOnlyDigits(ref password));
+        }
+
+        [TestMethod]
+        public void CalculateByOnlyLetters_()
+        {
+            string password = "aabbcc";
+            int sum = -6;
+            Assert.AreEqual(sum, Program.CalculateByOnlyLetters(ref password));
+
+
+            password = "";
+            sum = 0;
+            Assert.AreEqual(sum, Program.CalculateByOnlyLetters(ref password));
+        }
+
+        [TestMethod]
+        public void CalculateByReapetedCharacters_()
+        {
+            string password = "abcd1a";
+            int sum = -2;
+            Assert.AreEqual(sum, Program.CalculateByReapetedCharacters(ref password));
+
+
+            password = "";
+            sum = 0;
+            Assert.AreEqual(sum, Program.CalculateByReapetedCharacters(ref password));
+        }
+    }
+}
---
+++ lab3/PasswordStrength/PasswordStrength/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PasswordStrength
+{
+    public class Program
+    {
+        public static int CalculateByLenght( ref string password)
+        {
+            return 4 * password.Length;
+        }
+
+        public static int CalculateByDigitsAmount( ref string password)
+        {
+            var digitsAmount = password.Where((n) => n >= '0' && n <= '9').Count();
+            return digitsAmount *= 4;
+        }
+
+        public static int CalculateByUpperLettersAmount( ref string password)
+        {
+            var upperLettersAmount = password.Where((n) => n >= 'A' && n <= 'Z').Count();
+            if (upperLettersAmount != 0)
+            {
+                return (password.Length - upperLettersAmount) * 2;
+            }
+            return 0;
+        }
+
+        public static int CalculateLowerCaseLettersAmount( ref string password)
+        {
+            var lowerLettersAmount = password.Where((n) => n >= 'a' && n <= 'z').Count();
+            if (lowerLettersAmount != 0)
+            {
+                return (password.Length - lowerLettersAmount) * 2;
+            }
+            return 0;
+        }
+
+        public static int CalculateByOnlyDigits( ref string password)
+        {
+            var digits = password.Where((n) => n >= '0' && n <= '9').Count();
+            if (digits == password.Length)
+            {
+                return -digits;
+            }
+            return 0;
+        }
+
+        public static int CalculateByOnlyLetters( ref string password)
+        {
+            var lettersAmount = password.Count(char.IsLetter);
+            if (lettersAmount == password.Length)
+            {
+                return -lettersAmount;
+            }
+            return 0;
+        }
+
+        public static int CalculateByReapetedCharacters(ref string password)
+        {
+            int numberOfDuplicates = 0;
+            Dictionary<char, int> dictionary = new Dictionary<char, int>();
+            foreach (char ch in password)
+            {
+                if (dictionary.ContainsKey(ch))
+                {
+                    dictionary[ch]++;
+                }
+                else
+                {
+                    dictionary.Add(ch, 1);
+                }
+            }
+            foreach (var item in dictionary)
+            {
+                if (item.Value > 1)
+                {
+                    numberOfDuplicates -= item.Value;
+                }
+            }
+            return numberOfDuplicates;
+        }
+
+        public int PasswordStength( ref string password)
+        {
+            int passwordStength = 0;
+
+            passwordStength += CalculateByLenght(ref password);
+            passwordStength += CalculateByUpperLettersAmount(ref password);
+            passwordStength += CalculateLowerCaseLettersAmount(ref password);
+            passwordStength += CalculateByOnlyDigits(ref password);
+            passwordStength += CalculateByOnlyLetters(ref password);
+            passwordStength += CalculateByReapetedCharacters(ref password);
+
+            return passwordStength;
+        }
+
+        public static int Main(string[] args)
+        {
+            if(args.Length != 1)
+            {
+                Console.WriteLine("Ошибка ввода комадной строки");
+                Console.WriteLine("Параметры комадной строки: <name.exe> <parametr>");
+                return 1;
+            }
+
+            string password = args[0];
+
+
+
+            return 0;
+        }
+    }
+}
