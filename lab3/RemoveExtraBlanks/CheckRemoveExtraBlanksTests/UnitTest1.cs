eb67501919211ef8765d694e4233e020d993771e
dmitrii <VoronindimTvink123@mail.ru>
2020-04-04 17:37:29 +0300

    add RemoveExtraBlanks

---
+++ lab3/RemoveExtraBlanks/CheckRemoveExtraBlanksTests/UnitTest1.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RemoveExtraBlanks;
+
+namespace CheckRemoveExtraBlanksTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void RemoveExtraBlanks_String_WithExtraBlanks()
+        {
+            string str = "\tHello    World\t";
+            string resultStr = "Hello World";
+            Assert.AreEqual(resultStr, Program.RemoveExtraBlanks(ref str));
+        }
+
+        [TestMethod]
+        public void RemoveExtraBlanks_String_WithoutExtraBlanks()
+        {
+            string str = "Hello World";
+            string resultStr = "Hello World";
+            Assert.AreEqual(resultStr, Program.RemoveExtraBlanks(ref str));
+        }
+    }
+}
---
+++ lab3/RemoveExtraBlanks/RemoveExtraBlanks/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace RemoveExtraBlanks
+{
+    public class Program
+    {
+        public static string RemoveExtraBlanks( ref string str)
+        {
+            str = str.Trim();
+            str = System.Text.RegularExpressions.Regex.Replace(str, "\\s+|\\t+", " ");
+            return str;
+        }
+
+        public static bool CopyFileWithRemoveExtraBlanks( ref string inputFileName, ref string outputFileName) 
+        {
+            if (!File.Exists(inputFileName))
+            {
+                Console.WriteLine("Ошибка открытия input файла");
+                return false;
+            }
+            using StreamReader inputFile = new StreamReader(inputFileName, encoding: Encoding.UTF8);
+
+            using StreamWriter outputFile = new StreamWriter(outputFileName, append: false, encoding: Encoding.UTF8);
+
+
+            string str;
+            while((str = inputFile.ReadLine()) != null)
+            {
+                RemoveExtraBlanks(ref str);
+                outputFile.WriteLine(str);
+            }
+            return true;
+        }
+
+        static int Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Параметры комадной строки: <name.exe> <input.txt> <output.txt>");
+                return 1;
+            }
+            string inputFileName = args[1];
+            string outputFileName = args[2];
+
+            if (!CopyFileWithRemoveExtraBlanks(ref inputFileName, ref outputFileName))
+            {
+                return 1;
+            }
+        
+            return 0;
+        }
+    }
+}
